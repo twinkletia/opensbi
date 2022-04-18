73df21f7dd9424fbb523ab5cbb31d82c84d1d9dd
twinkletia <vamosidemochumjetze215@gmail.com>
2022-04-18 14:04:56 +0900

    add shellscript of building linux for rv32xSoC

---
+++ makeLinux.sh
@@ -0,0 +1,6 @@
+make CROSS_COMPILE=riscv32-unknown-elf- PLATFORM=rv32xsoc FW_PAYLOAD_PATH=../../linux/arch/riscv/boot/Image FW_FDT_PATH=../software/bootrom/rv32xsoc.dtb
+make -C ../../simulation
+cp ../../simulation/bootrom.hex ./
+cp ../../simulation/rv32x_simulation ./
+cp ../../simulation/gtk.sh ./
+cp ../../simulation/rv32x.gtkw ./
